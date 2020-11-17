2b802465626bdf73afcf9f41c765cff39ce89562
WorldWideWebster <SeanMWebster@live.com>
2020-11-17 03:12:06 -0500

    Bullet phsycis includes link and build and run! Yay!

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 
 #include "main_defs.h"
 #include "libs.h"
-#include <bullet/btBulletDynamicsCommon.h>
+#include <bullet3/src/btBulletDynamicsCommon.h>
 
 
 // timing
@@ -41,7 +41,7 @@ int main()
 	UIDataWindow dataWindow("Data Window", renderer.getActiveScene());
 
 	/// Initialize Physics World
-	btBroadphaseInterface* broadphase = new btDbvtBroadphase();
+//	btBroadphaseInterface* broadphase = new btDbvtBroadphase();
 
 	glDisable( GL_CULL_FACE );
 	UITestWindow *testWindow = new UITestWindow(&show_demo_window, &show_render_window, &noise_map_viewer, &show_scene_window);
